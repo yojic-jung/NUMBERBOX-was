8f10e54c049d75b2c0754145e14e0f6747fe2207
yojic <dywlr74@naver.com>
2022-07-26 04:04:32 +0900

    ppt

--- src/main/java/com/numberbox/common/util/CommonUtil.java
+++ src/main/java/com/numberbox/common/util/CommonUtil.java
@@ -34,11 +34,6 @@ public class CommonUtil {
 		if(onlyOne) {
 			final BufferedImage img = new BufferedImage(pgsize.width, pgsize.height, BufferedImage.SCALE_SMOOTH);
 	        final Graphics2D graphics = img.createGraphics();
-	        GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
-	        Font[] fonts = e.getAllFonts();
-	        for (Font font : fonts) {
-	          System.out.println(font.getFontName());
-	        }
 	        //clear the drawing area
 	        graphics.setPaint(Color.white);
 	        graphics.fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height));
