e10da0761a48f0e7d99ee321f4f81f41a6ccae18
yojicjung <dywlr74@naver.com>
2024-09-05 20:44:25 +0900

    프로젝트 모듈화

---
+++ project/user-interface/rest-api/src/main/kotlin/com/numberbox/ui/rest_api/controller/members/MembersRegisterController.kt
@@ -0,0 +1,4 @@
+package com.numberbox.ui.rest_api.controller.members
+
+class MembersRegisterController {
+}
---
+++ project/user-interface/rest-api/src/main/kotlin/com/numberbox/ui/rest_api/util/response/ResponseData.kt
@@ -0,0 +1,4 @@
+package com.numberbox.ui.rest_api.util.response
+
+object ResponseData {
+}
