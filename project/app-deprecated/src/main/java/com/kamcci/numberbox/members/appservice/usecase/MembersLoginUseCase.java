59046fa27a64d015473a62badb684ad648e010d0
yojicjung <dywlr74@naver.com>
2024-12-27 22:37:18 +0900

    구버전 소스 제거

--- project/app-deprecated/src/main/java/com/kamcci/numberbox/members/appservice/usecase/MembersLoginUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.kamcci.numberbox.members.appservice.usecase;
-
-import com.kamcci.numberbox.members.restapi.dto.response.SignUpResultDto;
-
-public interface MembersLoginUseCase {
-    SignUpResultDto login(String requestEmail);
-}
--- project/app-deprecated/src/main/java/com/kamcci/numberbox/members/appservice/usecase/MembersRegisterUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.kamcci.numberbox.members.appservice.usecase;
-
-import com.kamcci.numberbox.members.restapi.dto.request.MembersRequest;
-import com.kamcci.numberbox.members.restapi.dto.response.SignUpResultDto;
-
-public interface MembersRegisterUseCase {
-    SignUpResultDto signUp(MembersRequest membersRequest);
-}
