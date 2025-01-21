d46f230ff0530aa911087bdce7f6f1b33404f08b
yojicjung <dywlr74@naver.com>
2025-01-21 09:00:49 +0900

    hwp to html 변환 기능 todo 작

--- project/user-interface/rest-api/src/main/kotlin/com/kamcci/numberbox/restapi/controller/hwp/HwpConvertController.kt
+++ project/user-interface/rest-api/src/main/kotlin/com/kamcci/numberbox/restapi/controller/hwp/HwpConvertController.kt
@@ -39,6 +39,14 @@ class HwpConvertController(
         val extension = hwpFile.originalFilename!!.split(".").last()
         val extensionType = HwpExtensionType.valueOf(extension.uppercase())
         val zipByteArr = hwpSocketClient.requestHtmlZip(hwpFile.inputStream, hwpFile.size.toInt(), extensionType)
+
+        /**
+         * todo
+         * 1. unzip
+         * 2. /bindata 하위 이미지 파일 s3 저장
+         * 3. /index.xhtml 한줄씩 읽어들여 문자열로 컨텐츠 저장 -> hwp_convert_content에 영속화
+         * 4. s3FileUrl, contentsList(나의 변환 컨텐츠 목록)
+         */
         return ResponseUtil.ok(mapOf("zipFile" to Base64.getEncoder().encodeToString(zipByteArr)))
     }
 }
