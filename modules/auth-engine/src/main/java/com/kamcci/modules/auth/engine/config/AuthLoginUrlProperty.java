cbe1f882c5fa05707c6cb0495a3636d5f56b872d
yojicjung <dywlr74@naver.com>
2025-06-21 11:58:49 +0900

    monitoring 설정 추가

--- modules/auth-engine/src/main/java/com/kamcci/modules/auth/engine/config/AuthLoginUrlProperty.java
+++ modules/auth-engine/src/main/java/com/kamcci/modules/auth/engine/config/AuthLoginUrlProperty.java
@@ -2,6 +2,8 @@ package com.kamcci.modules.auth.engine.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 @ConfigurationProperties(prefix = "kamcci.auth.login.url")
-public record AuthLoginUrlProperty(String process, String fail, String logout) { }
+public record AuthLoginUrlProperty(String process, String fail, String logout, List<String> except) { }
 
