38120c94959df26f64f551d6044974a7ada0ed45
yojic-jung <dywlr74@naver.com>
2025-06-27 10:21:01 +0900

    teamcity test

--- project/infrastructure/orm-jpa-adapter/src/testFixtures/kotlin/com/kamcci/numberbox/infra/orm/jpa/adapter/config/MysqlTCExtension.kt
+++ project/infrastructure/orm-jpa-adapter/src/testFixtures/kotlin/com/kamcci/numberbox/infra/orm/jpa/adapter/config/MysqlTCExtension.kt
@@ -2,12 +2,14 @@ package com.kamcci.numberbox.infra.orm.jpa.adapter.config
 
 import org.junit.jupiter.api.extension.Extension
 import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.containers.Network
 
 class MysqlTCExtension : Extension {
     companion object {
         var mysqlContainer: MySQLContainer<Nothing> =
             MySQLContainer<Nothing>("mysql:8.3.0")
                 .apply {
+                    this.withNetwork(Network.SHARED)
                     this.withDatabaseName("numberbox_tc")
                     this.withUsername("dywlr")
                     this.withPassword("1111")
